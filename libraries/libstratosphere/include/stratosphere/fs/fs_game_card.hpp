2dda8e3550b806e8ec06e794f84662cb4f0ae650
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2020-11-16 08:42:48 +0100

    meme xci
    my first try to make a xci meme loader

--- libraries/libstratosphere/include/stratosphere/fs/fs_game_card.hpp
+++ libraries/libstratosphere/include/stratosphere/fs/fs_game_card.hpp
@@ -39,6 +39,54 @@ namespace ams::fs {
         DifferentRegionCupToGlobalDeviceFlag = (1 << 4),
     };
 
+#define MAGIC_HEAD 0x44414548 /* "HEAD" */
+#define MAGIC_HFS0 0x30534648 /* "HFS0" */
+
+    typedef struct
+    {
+        uint8_t header_sig[0x100];
+        uint32_t magic;
+        uint32_t secure_offset;
+        uint32_t _0x108;
+        uint8_t _0x10C;
+        uint8_t cart_type;
+        uint8_t _0x10E;
+        uint8_t _0x10F;
+        uint64_t _0x110;
+        uint64_t cart_size;
+        unsigned char reversed_iv[0x10];
+        uint64_t hfs0_offset;
+        uint64_t hfs0_header_size;
+        unsigned char hfs0_header_hash[0x20];
+        unsigned char crypto_header_hash[0x20];
+        uint32_t _0x180;
+        uint32_t _0x184;
+        uint32_t _0x188;
+        uint32_t _0x18C;
+        unsigned char encrypted_data[0x70];
+    } GamecardHeader;
+    static_assert(sizeof(GamecardHeader) == 0x200, "GamecardHeader has incorrect size.");
+    
+    typedef struct 
+    {
+        uint32_t magic;
+        uint32_t num_files;
+        uint32_t string_table_size;
+        uint32_t reserved;
+    } HFS0BaseHeader;
+    static_assert(sizeof(HFS0BaseHeader) == 0x10, "HFS0BaseHeader must be 0x10");
+
+    typedef struct
+    {
+        uint64_t offset;
+        uint64_t size;
+        uint32_t string_table_offset;
+        uint32_t hashed_size;
+        uint64_t reserved;
+        unsigned char hash[0x20];
+    } HFS0FileEntry;
+    static_assert(sizeof(HFS0FileEntry) == 0x40, "HFS0FileEntry must be 0x18");
+
     using GameCardHandle = u32;
 
     Result GetGameCardHandle(GameCardHandle *out);
--- libraries/libstratosphere/source/fs/fs_game_card.cpp
+++ libraries/libstratosphere/source/fs/fs_game_card.cpp
@@ -16,42 +16,102 @@
 #include <stratosphere.hpp>
 #include "fsa/fs_mount_utils.hpp"
 
-namespace ams::fs {
+namespace ams::fs
+{
 
-    namespace {
+    namespace
+    {
 
-        const char *GetGameCardMountNameSuffix(GameCardPartition which) {
-            switch (which) {
-                case GameCardPartition::Update: return impl::GameCardFileSystemMountNameUpdateSuffix;
-                case GameCardPartition::Normal: return impl::GameCardFileSystemMountNameNormalSuffix;
-                case GameCardPartition::Secure: return impl::GameCardFileSystemMountNameSecureSuffix;
+        const char *GetGameCardMountNameSuffix(GameCardPartition which)
+        {
+            switch (which)
+            {
+            case GameCardPartition::Update:
+                return impl::GameCardFileSystemMountNameUpdateSuffix;
+            case GameCardPartition::Normal:
+                return impl::GameCardFileSystemMountNameNormalSuffix;
+            case GameCardPartition::Secure:
+                return impl::GameCardFileSystemMountNameSecureSuffix;
                 AMS_UNREACHABLE_DEFAULT_CASE();
             }
         }
 
-        class GameCardCommonMountNameGenerator : public fsa::ICommonMountNameGenerator, public impl::Newable {
-            private:
-                const GameCardHandle handle;
-                const GameCardPartition partition;
-            public:
-                explicit GameCardCommonMountNameGenerator(GameCardHandle h, GameCardPartition p) : handle(h), partition(p) { /* ... */ }
+        class GameCardCommonMountNameGenerator : public fsa::ICommonMountNameGenerator, public impl::Newable
+        {
+        private:
+            const GameCardHandle handle;
+            const GameCardPartition partition;
 
-                virtual Result GenerateCommonMountName(char *dst, size_t dst_size) override {
-                    /* Determine how much space we need. */
-                    const size_t needed_size = strnlen(impl::GameCardFileSystemMountName, MountNameLengthMax) + strnlen(GetGameCardMountNameSuffix(this->partition), MountNameLengthMax) + sizeof(GameCardHandle) * 2 + 2;
-                    AMS_ABORT_UNLESS(dst_size >= needed_size);
+        public:
+            explicit GameCardCommonMountNameGenerator(GameCardHandle h, GameCardPartition p) : handle(h), partition(p)
+            { /* ... */
+            }
 
-                    /* Generate the name. */
-                    auto size = std::snprintf(dst, dst_size, "%s%s%08x:", impl::GameCardFileSystemMountName, GetGameCardMountNameSuffix(this->partition), this->handle);
-                    AMS_ASSERT(static_cast<size_t>(size) == needed_size - 1);
+            virtual Result GenerateCommonMountName(char *dst, size_t dst_size) override
+            {
+                /* Determine how much space we need. */
+                const size_t needed_size = strnlen(impl::GameCardFileSystemMountName, MountNameLengthMax) + strnlen(GetGameCardMountNameSuffix(this->partition), MountNameLengthMax) + sizeof(GameCardHandle) * 2 + 2;
+                AMS_ABORT_UNLESS(dst_size >= needed_size);
 
-                    return ResultSuccess();
-                }
+                /* Generate the name. */
+                auto size = std::snprintf(dst, dst_size, "%s%s%08x:", impl::GameCardFileSystemMountName, GetGameCardMountNameSuffix(this->partition), this->handle);
+                AMS_ASSERT(static_cast<size_t>(size) == needed_size - 1);
+
+                return ResultSuccess();
+            }
         };
 
+    } // namespace
+    constexpr const char *const NCMSdMountName = "#NCMsdpatch";
+    os::Mutex g_ldr_sd_lock(false);
+    bool g_mounted_sd;
+
+    bool EnsureSdCardMounted()
+    {
+        std::scoped_lock lk(g_ldr_sd_lock);
+
+        if (g_mounted_sd)
+        {
+            return true;
+        }
+
+        if (!cfg::IsSdCardInitialized())
+        {
+            return false;
+        }
+
+        if (R_FAILED(fs::MountSdCard(NCMSdMountName)))
+        {
+            return false;
+        }
+
+        return (g_mounted_sd = true);
     }
 
-    Result GetGameCardHandle(GameCardHandle *out) {
+    static inline HFS0FileEntry *hfs0_get_file_entry(HFS0BaseHeader *hdr, uint32_t i)
+    {
+        if (i >= hdr->num_files)
+            return NULL;
+        return (HFS0FileEntry *)((char *)(hdr) + sizeof(*hdr) + i * sizeof(HFS0FileEntry));
+    }
+
+    static inline char *hfs0_get_string_table(HFS0BaseHeader *hdr)
+    {
+        return (char *)(hdr) + sizeof(*hdr) + hdr->num_files * sizeof(HFS0FileEntry);
+    }
+
+    static inline uint64_t hfs0_get_header_size(HFS0BaseHeader *hdr)
+    {
+        return sizeof(*hdr) + hdr->num_files * sizeof(HFS0FileEntry) + hdr->string_table_size;
+    }
+
+    static inline char *hfs0_get_file_name(HFS0BaseHeader *hdr, uint32_t i)
+    {
+        return hfs0_get_string_table(hdr) + hfs0_get_file_entry(hdr, i)->string_table_offset;
+    }
+
+    Result GetGameCardHandle(GameCardHandle *out)
+    {
         /* TODO: fs::DeviceOperator */
         /* Open a DeviceOperator. */
         ::FsDeviceOperator d;
@@ -63,14 +123,120 @@ namespace ams::fs {
         return fsDeviceOperatorGetGameCardHandle(std::addressof(d), reinterpret_cast<::FsGameCardHandle *>(out));
     }
 
-    Result MountGameCardPartition(const char *name, GameCardHandle handle, GameCardPartition partition) {
+    Result Create(std::shared_ptr<fs::fsa::IFileSystem> *out,std::shared_ptr<fs::IStorage> storage) {
+        /* Allocate a filesystem. */
+        //std::shared_ptr fs = fssystem::AllocateShared<fssystem::Sha256PartitionFileSystem>();
+        //R_UNLESS(fs != nullptr, fs::ResultAllocationFailureInPartitionFileSystemCreatorA());
+
+        /* Initialize the filesystem. */
+        //R_TRY(fs->Initialize(std::move(storage)));
+
+        /* Set the output. */
+        //*out = std::move(fs);
+        return ResultSuccess();
+    }
+
+    std::shared_ptr<fs::fsa::IFileSystem> Secure1;
+    bool MountSDcard()
+    {
+        /* Vars */
+        char path[fs::EntryNameLengthMax + 1];
+
+        /* GameCard Image */
+        fs::IStorage *GamecardImage;
+
+        /* HFS0 Images */
+        fs::SubStorage *hfs0root;
+        fs::SubStorage *hfs0Update;
+        fs::SubStorage *hfs0Normal;
+        fs::SubStorage *hfs0Secure;
+        fs::SubStorage *hfs0Logo;
+
+        /* HFS0 Headers */
+        GamecardHeader xciheader;
+        HFS0BaseHeader rootheader;
+        HFS0BaseHeader Secureheader;
+
+        /* Root Partitions Entrys */
+        HFS0FileEntry UpdateEntry;
+        HFS0FileEntry NormalEntry;
+        HFS0FileEntry *SecureEntry;
+        HFS0FileEntry LogoEntry;
+
+        /* IFileSystem GameCard Partition */
+        
+        
+
+        /* Open the file. */
+        fs::FileHandle file;
+        std::snprintf(path, sizeof(path), "%s:/D3fOS/%s", NCMSdMountName, "meme.xci");
+        if (R_SUCCEEDED(fs::OpenFile(std::addressof(file), path, fs::OpenMode_Read)))
+        {
+            // Convert FileHandle to IStorage
+            auto tmp = std::shared_ptr<IStorage>(new FileHandleStorage(file));
+            GamecardImage = tmp.get();
+            // Read XCI header
+            GamecardImage->Read(0x0, std::addressof(xciheader), 0x200);
+            if (xciheader.magic == MAGIC_HEAD)
+            {
+                s64 GamecardImageSize;
+                GamecardImage->GetSize(&GamecardImageSize);
+                // Open root partition header as SubStorage
+                hfs0root = new fs::SubStorage(GamecardImage, xciheader.hfs0_offset, GamecardImageSize - xciheader.hfs0_offset);
+                // Read global header
+                hfs0root->Read(0x0, std::addressof(rootheader), sizeof(HFS0BaseHeader));
+                hfs0root->Read(0x0, std::addressof(rootheader), hfs0_get_header_size(&rootheader));
+                if (rootheader.magic == MAGIC_HFS0)
+                {
+                    SecureEntry = hfs0_get_file_entry(&rootheader, (uint32_t)fs::GameCardPartition::Secure);
+                    hfs0Secure = new fs::SubStorage(hfs0root, SecureEntry->offset, SecureEntry->size);
+                    hfs0Secure->Read(0x0, std::addressof(Secureheader), sizeof(HFS0BaseHeader));
+                    hfs0Secure->Read(0x0, std::addressof(Secureheader), hfs0_get_header_size(&rootheader));
+                    if (Secureheader.magic == MAGIC_HFS0) {
+                        auto meme = std::shared_ptr<IStorage>(hfs0Secure);
+                        Create(&Secure1,meme);
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                fs::CloseFile(file);
+                return false;
+            }
+        }
+        else
+        {
+            fs::CloseFile(file);
+            return false;
+        }
+    }
+
+    Result MountGameCardPartition(const char *name, GameCardHandle handle, GameCardPartition partition)
+    {
         /* Validate the mount name. */
         R_TRY(impl::CheckMountNameAllowingReserved(name));
 
-        /* Open gamecard filesystem. This uses libnx bindings. */
         ::FsFileSystem fs;
         const ::FsGameCardHandle _hnd = {handle};
-        R_TRY(fsOpenGameCardFileSystem(std::addressof(fs), std::addressof(_hnd), static_cast<::FsGameCardPartition>(partition)));
+
+        /* Open gamecard filesystem. This uses libnx bindings. */
+        if (!EnsureSdCardMounted())
+        {
+            return 0;
+        }
+        else
+        {
+            if (MountSDcard() == true) {
+                R_TRY(fsOpenGameCardFileSystem(std::addressof(fs), std::addressof(_hnd), static_cast<::FsGameCardPartition>(partition)));
+            }
+        }
+
+        fssystem::AllocateShared<fssystem::Sha256PartitionFileSystem>();
 
         /* Allocate a new filesystem wrapper. */
         auto fsa = std::make_unique<RemoteFileSystem>(fs);
@@ -84,4 +250,4 @@ namespace ams::fs {
         return fsa::Register(name, std::move(fsa), std::move(generator));
     }
 
-}
+} // namespace ams::fs
